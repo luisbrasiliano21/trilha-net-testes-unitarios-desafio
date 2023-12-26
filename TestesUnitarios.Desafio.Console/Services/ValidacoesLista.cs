70730ccc1a937408892342059b135c37b630b331
Luis Brasiliano <luiskevinibn@gmail.com>
2023-12-26 10:32:22 -0300

    Desafio de testes, concluido.

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -1,3 +1,4 @@
+
 namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesLista
@@ -29,5 +30,7 @@ namespace TestesUnitarios.Desafio.Console.Services
         {
             return lista.Min();
         }
+
+        
     }
 }
